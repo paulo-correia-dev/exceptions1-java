e110bf8f34e8498ccccfb1668a3cc2e6f8c9fb87
Paulo Correia <programmerpaulo@gmail.com>
2023-10-04 23:11:06 -0300

    versao boa

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,55 +5,50 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import model.entities.Reserva;
+import model.exceptions.DominioException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
-		System.out.print("Número do quarto: ");
-		int numeroQuarto = sc.nextInt();
-		
-		System.out.print("Dia do checkin: ");
-		LocalDate checkin = LocalDate.parse(sc.next(), fmt1);
-		
-		System.out.print("Dia do checkout: ");
-		LocalDate checkout = LocalDate.parse(sc.next(), fmt1);
-		
-		if(!checkout.isAfter(checkin)) {
+			Scanner sc = new Scanner(System.in);
+			DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 			
-			System.out.println("Erro na reserva: Data de checkou anterior a data de checkin.");
-			
-		}
-		else {
-			
+			try {
+			System.out.print("Número do quarto: ");
+			int numeroQuarto = sc.nextInt();
+	
+			System.out.print("Dia do checkin: ");
+			LocalDate checkin = LocalDate.parse(sc.next(), fmt1);
+	
+			System.out.print("Dia do checkout: ");
+			LocalDate checkout = LocalDate.parse(sc.next(), fmt1);
+	
 			Reserva reserva = new Reserva(numeroQuarto, checkin, checkout);
 			System.out.println(reserva);
-			
+	
 			System.out.println();
 			System.out.println("Entre com os dados novamente para atualizar a reserva:");
-			
+	
 			System.out.print("Dia do checkin: ");
 			checkin = LocalDate.parse(sc.next(), fmt1);
-			
+	
 			System.out.print("Dia do checkout: ");
-			checkout = LocalDate.parse(sc.next(), fmt1);		
-			
-			String erro = reserva.atualizarData(checkin, checkout);
-			if(erro != null) {
-				
-				System.out.println("Erro na reserva: " + erro);
-			}
-			else {
-				
+			checkout = LocalDate.parse(sc.next(), fmt1);
+	
+			reserva.atualizarData(checkin, checkout);
+	
 			System.out.println(reserva);
-			
-			}
-			
-			
 		}
+		catch (DominioException e) {
+			
+			System.out.println(e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Erro inesperado!");
+		}
+		
+		sc.close();
 
 	}
 
--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -1,19 +1,26 @@
 package model.entities;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+
+import model.exceptions.DominioException;
 
 public class Reserva {
 
 	private Integer numeroQuarto;
 	private LocalDate checkin;
 	private LocalDate checkout;
-	
+
 	private static DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	public Reserva(Integer numeroQuarto, LocalDate checkin, LocalDate checkout) {
+	public Reserva(Integer numeroQuarto, LocalDate checkin, LocalDate checkout){
+
+		if (!checkout.isAfter(checkin)) {
+
+			throw new DominioException("Erro na reserva: Data de checkou anterior a data de checkin.");
+		}
+
 		this.numeroQuarto = numeroQuarto;
 		this.checkin = checkin;
 		this.checkout = checkout;
@@ -39,36 +46,27 @@ public class Reserva {
 
 		Period dif = Period.between(checkin, checkout);
 		return dif.getDays();
-		
+
 	}
-	
-	public String atualizarData(LocalDate checkin, LocalDate checkout) {
-		
+
+	public void atualizarData(LocalDate checkin, LocalDate checkout){
+
 		LocalDate hoje = LocalDate.now();
-		
-		if(checkin.isBefore(hoje) || checkout.isBefore(hoje)) {
-			
-			return "Erro na reserva: As datas para atualização devem ser futuras.";
+
+		if (checkin.isBefore(hoje) || checkout.isBefore(hoje)) {
+
+			throw new DominioException("Erro na reserva: As datas para atualização devem ser futuras.");
 		}
-		
-		if(!checkout.isAfter(checkin)) {
-			
-			return "Erro na reserva: Data de checkou anterior a data de checkin.";
-			
-		}
-		
+
 		this.checkin = checkin;
 		this.checkout = checkout;
-		
-		return null;
-		
+
 	}
 
 	@Override
 	public String toString() {
-		return "Reserva: numeroQuarto= " + numeroQuarto + ", checkin= " + fmt1.format(checkin) + ", checkout= " + fmt1.format(checkout) + ", duração= " + duracao() + " noites.";
+		return "Reserva: numeroQuarto= " + numeroQuarto + ", checkin= " + fmt1.format(checkin) + ", checkout= "
+				+ fmt1.format(checkout) + ", duração= " + duracao() + " noites.";
 	}
-	
-	
-	
+
 }
